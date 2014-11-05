141c4552ab7351e8751042a1a742546f87044994
Ben Gundersen <ben@bengundersen.com>
2014-11-05 00:50:53 -0500

    test

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var port    = process.env.PORT || 5000;
 server.get('/', function(req, res) {
   res.send("bgun.me");
 });
+// wildcard catching all non-matched requests
+server.get('*', function(req, res) {
+  res.send(404, "<h1>四零四</h1>");
+});
 
 console.log("Listening on port "+port);
 server.listen(port);
